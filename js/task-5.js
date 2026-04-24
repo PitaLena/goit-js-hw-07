e15fc2fd45278514a901e51f945feae2c12f7829
PitaLena <pita261991@gmail.com>
2026-04-24 20:17:20 +0300

    goit-js-hw-07

---
+++ js/task-5.js
@@ -0,0 +1,17 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const btn = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+const body = document.body;
+
+if (btn && colorSpan) {
+  btn.addEventListener('click', () => {
+    const color = getRandomHexColor();
+    body.style.backgroundColor = color;
+    colorSpan.textContent = color;
+  });
+}
